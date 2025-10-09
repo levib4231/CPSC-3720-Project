e107d7e80de6501fd68197259e8130543d1669f1
levib4231 <lrbates@clemson.edu>
2025-10-09 14:09:02 -0400

    Updated gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 
+
 function App() {
   const [events, setEvents] = useState([]);
   const [statusMessage, setStatusMessage] = useState('');
